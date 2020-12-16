b502c0a20d3bd4913568dcea7b369aa954569484
Florian Jung <flo@windfis.ch>
2020-12-16 19:23:14 +0100

    renaming support

--- README.md
+++ README.md
@@ -10,16 +10,14 @@ In case of a full system dump, each patch file is named by its number and its
 name. The files created can be sent to the Monologue using `amidi -p hw:1 -s
 file.syx`, for example.  The files in the output directory will *not* be
 automatically stored in the Monologue's memory.  If you want this behaviour,
-use the files in the `outdir/auto_write/` subdirectory instead.  You can
-concatenate files in `outdir/auto_write/` at will, to restore a subset of
-patches automatically.
+use the files in the `outdir/auto_write/` subdirectory instead.
 
 ## Usage
 
 To create a sysex dump:
 
 ```
-amidi -p hw:1 -r dump.syx   # then select "dump patch", wait, and press CTRL+C
+amidi -p hw:1,0,1 -r dump.syx   # then select "dump patch", wait, and press CTRL+C
 ```
 
 To parse and split a sysex dump:
@@ -31,6 +29,45 @@ monologue_extractor.py file.sysex outdir/
 To send back a single patch to the Monologue:
 
 ```
-amidi -p hw:1 -s outdir/003_Anfem.syx # will not automatically write the Monologue's memory
-amidi -p hw:1 -s outdir/auto_write/003_Anfem.syx # will automatically write the Monologue's memory
+amidi -p hw:1,0,1 -s outdir/003_Anfem.syx # will not automatically write the Monologue's memory
+amidi -p hw:1,0,1 -s outdir/auto_write/003_Anfem.syx # will automatically write the Monologue's memory
 ```
+
+## Example: Renaming patches
+
+Create the full dump:
+
+```
+amidi -p hw:1,0,1 -r dump.syx   # then select "dump patch", wait, and press CTRL+C
+monologue_extractor.py dump.syx outdir/
+```
+
+Then, conveniently rename your patches:
+
+```
+cd outdir/auto_write/
+monologue_extract.py -r 093_Init\ Program.syx 'Warm Bass'
+monologue_extract.py -r 094_Init\ Program.syx 'Glitch'
+monologue_extract.py -r 095_Init\ Program.syx '5th BassLead'
+monologue_extract.py -r 096_Init\ Program.syx 'Accordeon'
+monologue_extract.py -r 097_\<afx\ acid3\>.syx "5th Stab"
+monologue_extract.py -r 098_Init\ Program.syx 'My Lead'
+monologue_extract.py -r 099_Init\ Program.syx Brass
+monologue_extract.py -r 100_Init\ Program.syx 'Accordeon'
+```
+
+To send them back to the synth in bulk, use:
+
+```
+for i in 09* 100*; do amidi -p hw:1,0,1 -s "$i"; sleep 0.2; done
+```
+
+You can monitor progress during sending by watching `amidi -p hw:1,0,1 -d` output.
+The status messages are:
+
+```
+F0 42 30 00 01 44 24 F7 # error
+F0 42 30 00 01 44 23 F7 # success
+```
+
+
--- monologue_extract.py
+++ monologue_extract.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 # Copyright (c) 2020, Florian Jung (flo@windfis.ch)
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -47,36 +47,68 @@ def get_name(data):
 			result += bytes([b]).decode('ascii')
 	return result
 
-if len(sys.argv) != 3:
+def set_name(data, offset, name):
+	name = bytes(name, 'ascii') + bytes([0]*12)
+	name = name[0:12]
+	return data[0:offset+0] + name[0:3] + data[offset+3: offset+4] + name[3:10] + data[offset+11: offset+12] + name[10:12] + data[offset+14:]
+
+if not (len(sys.argv) == 3 or (len(sys.argv) == 4 and sys.argv[1] == '-r')):
 	print("""Korg Monologue Sysex Extractor.
 Reads a sysex dump from a Korg Monologue synthesizer and extracts all
 patches, properly names, into outdir/. Can also be used on a single
 patch dump, which will be renamed accordingly and copied to outdir/.
 
+It can rename patches using the -r option.
+
 Dumps can be created using `amidi -p hw:1 -r dump.syx`.
 
-Usage: %s file.sysex outdir/""" % sys.argv[0])
+Usage: %s file.sysex outdir/
+       %s -r file.sysex <new patch name>
 
-data = open(sys.argv[1], 'rb').read()
-path = sys.argv[2]
+Note: Patch names are limited to 12 characters.""" % (sys.argv[0], sys.argv[0]))
+	exit(1)
 
-if not os.path.exists(path):
-	os.makedirs(path)
 
-SYSEX_BEGIN = bytes([0xF0])
-messages = [ SYSEX_BEGIN + part for part in data.split(SYSEX_BEGIN)[1:] ]
 
-for message in messages:
-	if ex(message, [0,6,8,9,10,11,519]) == [0xf0, 0x40, 0x50, 0x52, 0x4f, 0x47, 0xf7]:
-		print(get_name(message[12:]).strip())
-		filename = "%s.syx" % get_name(message[12:]).strip()
-		open(path + "/" + filename, 'wb').write(message)
+if len(sys.argv) == 3:
+	data = open(sys.argv[1], 'rb').read()
+	path = sys.argv[2]
 
-	elif ex(message, [0,6,10,11,12,13,521]) == [0xf0, 0x4c, 0x50, 0x52, 0x4f, 0x47, 0xf7]:
-		print(get_name(message[14:]).strip())
-		filename = "%03d_%s.syx" % (message[7]+1, get_name(message[14:]).strip())
-		if not os.path.exists(path + "/auto_write"): os.makedirs(path + "/auto_write")
-		open(path + "/auto_write/" + filename, 'wb').write(message)
-		open(path + "/" + filename, 'wb').write(no_auto_write(message))
+	if not os.path.exists(path):
+		os.makedirs(path)
+
+	SYSEX_BEGIN = bytes([0xF0])
+	messages = [ SYSEX_BEGIN + part for part in data.split(SYSEX_BEGIN)[1:] ]
+
+	for message in messages:
+		if ex(message, [0,6,8,9,10,11,519]) == [0xf0, 0x40, 0x50, 0x52, 0x4f, 0x47, 0xf7]:
+			print(get_name(message[12:]).strip())
+			filename = "%s.syx" % get_name(message[12:]).strip()
+			open(path + "/" + filename, 'wb').write(message)
+
+		elif ex(message, [0,6,10,11,12,13,521]) == [0xf0, 0x4c, 0x50, 0x52, 0x4f, 0x47, 0xf7]:
+			print(get_name(message[14:]).strip())
+			filename = "%03d_%s.syx" % (message[7]+1, get_name(message[14:]).strip())
+			if not os.path.exists(path + "/auto_write"): os.makedirs(path + "/auto_write")
+			open(path + "/auto_write/" + filename, 'wb').write(message)
+			open(path + "/" + filename, 'wb').write(no_auto_write(message))
+		else:
+			print("<ignored>")
+			open(path + "/auto_write/999-epilogue.syx", 'ab').write(message)
+else:
+	data = open(sys.argv[2], 'rb').read()
+	name = sys.argv[3]
+
+	name = name[0:12]
+	
+	if ex(data, [0,6,8,9,10,11,519]) == [0xf0, 0x40, 0x50, 0x52, 0x4f, 0x47, 0xf7]:
+		old_name = get_name(data[12:]).strip()
+		data = set_name(data, 12, name)
+	elif ex(data, [0,6,10,11,12,13,521]) == [0xf0, 0x4c, 0x50, 0x52, 0x4f, 0x47, 0xf7]:
+		old_name = get_name(data[14:]).strip()
+		data = set_name(data, 14, name)
 	else:
-		print("<ignored>")
+		print("Unknown file %s, exiting." % sys.argv[1])
+		exit(1)
+	print("Renamed from '%s' to '%s'" % (old_name, name))
+	open(sys.argv[2], 'wb').write(data)
